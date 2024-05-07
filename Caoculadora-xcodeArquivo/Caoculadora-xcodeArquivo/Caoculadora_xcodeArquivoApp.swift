9fb94881054fdf0a2d0412c730aedbca715a2dd5
Marina-Paixao <marina.paixao@ufpe.br>
2024-05-07 11:31:02 -0300

    Cria projeto de Caoculadora no Xcode

---
+++ Caoculadora-xcodeArquivo/Caoculadora-xcodeArquivo/Caoculadora_xcodeArquivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Caoculadora_xcodeArquivoApp.swift
+//  Caoculadora-xcodeArquivo
+//
+//  Created by Marina Meirelles Paixão on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Caoculadora_xcodeArquivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caoculadora-xcodeArquivo/Caoculadora-xcodeArquivo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caoculadora-xcodeArquivo
+//
+//  Created by Marina Meirelles Paixão on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
